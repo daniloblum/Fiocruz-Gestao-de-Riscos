162710b125145cc6050b5e164cfb4720ca999934
Danilo Blum <dnl.blm@gmail.com>
2021-08-27 20:45:50 -0300

    modulo1 sidebar, recursos visuais modal
    modulo1 > aula1: sidebar com titulos e link de resumo em video
    modulo1: header
    recursos-visuais: modal de zoom de imagem e exibicao de video

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,59 @@
 // Ajustar a altura da página quando for menor que a sidebar
 
-$(document).ready( function () {
-    
-    const alturaSidebar = $(".sidebar").height()
-    const alturaContent = $(".content").height()
-    const alturaConteudo = $(".header").height() + $("#page-content").height() + $("footer").height()
+// $(document).ready( function () {
 
-    // console.log(alturaSidebar)
-    // console.log(alturaContent)
-    // console.log(alturaConteudo)
+(function () {
+  let sidebarHeight = document.getElementsByClassName('sidebar');
+  let contentHeight = document.getElementsByClassName('content');
+  let headerHeight = document.getElementsByTagName('header');
+  let footerHeight = document.getElementsByTagName('footer');
+  let sectionsToDiscount =
+    headerHeight[0].offsetHeight + footerHeight[0].offsetHeight;
+  // const sidebarHeight = $(".sidebar").height()
+  // const contentHeight = $(".content").height()
+  // const sectionsToDiscount = ($(".header").height() + 2 * $("footer").height())
 
-    if(alturaSidebar > alturaConteudo) {
+  // console.log(sidebarHeight[0].offsetHeight)
+  // console.log(contentHeight[0].offsetHeight)
+  // console.log(sectionsToDiscount)
 
-        // console.log("é maior")
+  if (sidebarHeight[0].offsetHeight > contentHeight[0].offsetHeight) {
+    //
 
-        $("#page-content").height($(".sidebar").height() - ($(".header").height() + 2 * $("footer").height()))
-
-        // console.log("nova altura do conteudo: ", $("#page-content").height())
-
-    }
-})
+    let pageContent = document.getElementById('page-content');
+    pageContent.style.minHeight =
+      sidebarHeight[0].offsetHeight - sectionsToDiscount + 'px';
+    //($(".sidebar").height() - ($(".header").height() + 2 * $("footer").height()))
+  }
+})();
 
 // Módulo 1 / Aula 1 / Topico 5
 // Mapa do Brasil 1
 
 function toggleLegenda(item) {
-    var texto = document.getElementById(item)
+  var texto = document.getElementById(item);
 
-    if (texto.style.opacity == 0) {
-        texto.style.opacity = '1'
-    } else {
-        texto.style.opacity = '0'
-    }
+  if (texto.style.opacity == 0) {
+    texto.style.opacity = '1';
+  } else {
+    texto.style.opacity = '0';
+  }
 }
 
-function mapHover (item) {
-    var mapa = document.getElementById(item)
-    mapa.style.fill = "#b5ff61";
+function mapHover(item) {
+  var mapa = document.getElementById(item);
+  mapa.style.fill = '#b5ff61';
 }
 
 // Mapa do Brasil 1
 
-function mapHoverOut (item) {
-    var mapa = document.getElementById(item)
-    
-    if (item === "ba-mapa") {
-        mapa.style.fill = "#9cd15e";
-    }
-    if (item === "pe-mapa") {
-        mapa.style.fill = "#71a94a";
-    }
+function mapHoverOut(item) {
+  var mapa = document.getElementById(item);
+
+  if (item === 'ba-mapa') {
+    mapa.style.fill = '#9cd15e';
+  }
+  if (item === 'pe-mapa') {
+    mapa.style.fill = '#71a94a';
+  }
 }
