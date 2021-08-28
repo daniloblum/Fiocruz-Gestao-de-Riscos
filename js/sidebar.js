178382efc077b2e37126c70240de72771dc72a0a
Danilo Blum <dnl.blm@gmail.com>
2021-08-27 21:22:03 -0300

    acerto de video do modal no mobile, customização do menu t

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,57 +1,45 @@
 // DESKTOP
 
-
-
 // // MOBILE
 
-var sidebarBtn = $("#sidebar-toggle")
-var content = $(".content")
-var sidebarMobile = $(".sidebar")
+var sidebarBtn = $('#sidebar-toggle');
+var content = $('.content');
+var sidebarMobile = $('.sidebar');
 var btn = $('.btn');
 
 function touchControl(event, obj) {
-    if (event == true) {
-        obj.style.touchAction = "auto";
-    }
+  if (event == true) {
+    obj.style.touchAction = 'auto';
+  }
 }
 
-sidebarBtn.on("click", function () {
-   
-    
+sidebarBtn.on('click', function () {
+  if (sidebarMobile.hasClass('sidebar--open')) {
+    // closing
+    sidebarMobile.removeClass('sidebar--open');
+    sidebarMobile.addClass('sidebar--close');
 
-    if (sidebarMobile.hasClass("sidebar--open")) {
-        
-        // closing
-        sidebarMobile.removeClass("sidebar--open")
-        sidebarMobile.addClass("sidebar--close")
+    sidebarBtn.css('background-color', '#51ba6f');
 
-        sidebarBtn.css('background-color', '#3c7d1b')
-        
-        btn.toggleClass('active');
-        btn.toggleClass('not-active');
+    btn.toggleClass('active');
+    btn.toggleClass('not-active');
 
-        content.removeClass("touch-disable")
-        content.addClass("touch-enable")
-        
-    } else {
+    content.removeClass('touch-disable');
+    content.addClass('touch-enable');
+  } else {
+    // opening
+    sidebarMobile.removeClass('sidebar--close');
+    sidebarMobile.addClass('sidebar--open');
 
-        // opening
-        sidebarMobile.removeClass("sidebar--close")
-        sidebarMobile.addClass("sidebar--open")
+    sidebarBtn.css('background-color', '#dc3545');
 
-        sidebarBtn.css('background-color', '#917434')
+    btn.toggleClass('active');
+    btn.toggleClass('not-active');
 
-        btn.toggleClass('active');
-        btn.toggleClass('not-active');
+    content.removeClass('touch-enable');
+    content.addClass('touch-disable');
+  }
+});
 
-        content.removeClass("touch-enable")
-        content.addClass("touch-disable")
-        
-    }
-})
-
-if($(window).width()<768){
-
-   
-
+if ($(window).width() < 768) {
 }
